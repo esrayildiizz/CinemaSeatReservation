831b54fcd3bade924b06b409cd8a200e2876149f
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-24 20:23:51 +0300

    Son eklemeler yapildi ve hazir.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('seat:not(.reserved)');
 
+getFromLocalStorage();
+calculateTotal();
 
 //container içinde herhangi bir yere tıklandığında
 container.addEventListener('click', function(e) {
@@ -14,7 +17,6 @@ container.addEventListener('click', function(e) {
     //varsa silsin yoksa eklesin metodu:toggle
     e.target.classList.toggle('selected');
     calculateTotal();
-
   }
 });
 
@@ -22,11 +24,57 @@ select.addEventListener('change', function() {
   calculateTotal();
 });
 
-
 function calculateTotal() {
-  let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-  let price = select.value;
-  count.innerText = selectedSeatCount;
-  amount.innerText = selectedSeatCount * price;
+  const selectedSeats = container.querySelectorAll('.seat.selected');
 
+  const selectedSeatsArr = [];
+  const seatsArr = [];
+
+  selectedSeats.forEach(function(seat) {
+    selectedSeatsArr.push(seat);
+  });
+
+  seats.forEach(function(seat) {
+    seatsArr.push(push);
+  });
+
+  //seçili olan bilgilerin locale storage içerisine saklama.
+  //sayfayı yenilediğimizde bilgilerin halen durmasını istiyoruz.
+  //şimdi local storage içerisinde saklicak olduğumuz elemanı oluşturalım.
+  //örneğin [1,3,5] şeklinde seat lerin index numaralarını bulur
+  let selectedSeatIndexs = selectedSeatsArr.map(function(seat) {
+    return seatsArr.indexOf(seat);
+  });
+
+  let selectedSeatCount = selectedSeats.length;
+  count.innerText = selectedSeatCount;
+  amount.innerText = selectedSeatCount * select.value;
+
+
+  saveToLocalStorage(selectedSeatIndexs);
+}
+
+//localstorage den bilet bilgilerinin alınması.
+function getFromLocalStorage() {
+
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach(function(seat, index) {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
+
+//localstorage e kayıt edilmesi.
+function saveToLocalStorage(indexs) {
+  localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+  localStorage.setItem('selectedMovieIndex', select.selectedIndex);
 }
